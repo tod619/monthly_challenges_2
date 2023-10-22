bc34adfdcde3d3a1827c0c7500665e2de3894d99
tod619 <trevorodwyer619@hotmail.com>
2023-10-22 21:52:00 +0100

    import reverse + make urls dynamic

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Don't eat meat for the whole month",
@@ -25,7 +26,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Out of bounds!!")
     
     redirect_month = months[month - 1]
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
