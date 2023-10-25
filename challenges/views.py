07cf41d96e6fa14959396f81ef9a43e83bcbe6a4
tod619 <trevorodwyer619@hotmail.com>
2023-10-25 21:45:26 +0100

    create 404 page + rasie Http404 response

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 #from django.template.loader import render_to_string
 
@@ -59,7 +59,8 @@ def monthly_challenge(request, month):
         # response_data = render_to_string("challenges/challenge.html")
         # return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("<h1>Month not supported</h1>")
+        raise Http404()
+        # return HttpResponseNotFound("<h1>Month not supported</h1>")
 
 
 
